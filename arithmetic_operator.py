031b0ee301b9d7252f6b8976f178d3744ab79f25
sheddyranking <shedyisaac@gmail.com>
2022-01-13 23:57:58 +0100

    added arithmetic operators

---
+++ arithmetic_operator.py
@@ -0,0 +1,20 @@
+#General operator
+print(10/3)
+print(10+3)
+print(10-3)
+print(10*3)
+
+print(10//3) #returns an int
+print(10**3) #return exponential
+print(10%3) #returns remainder
+
+#argurmented 
+x= 10
+x = x+3 # will return 13
+print(x) 
+
+y = 10
+y +=3 # will still return 13
+print(y)
+
+#therefore it can be -=, *=, or  /= .
