e4769efc86c98f795ad7dcbd6e12072301e0d378
sheddyranking <shedyisaac@gmail.com>
2022-01-14 23:12:00 +0100

    added list

---
+++ list.py
@@ -0,0 +1,7 @@
+names = ["john", "bob", "jane", "james", "sam"]
+print(names) #returns all the names 
+print(names[0]) # return john
+print(names[0:3]) #return index of 0-2
+
+names[0] = 'jon' #modified john
+print (names)
