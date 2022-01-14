4a09d84ee0c6a46de733fcddc62218643f5ca318
sheddyranking <shedyisaac@gmail.com>
2022-01-14 23:29:53 +0100

    added list_methods

---
+++ list_methods.py
@@ -0,0 +1,16 @@
+numbers = [1, 2, 3, 4, 5, 6]
+print(numbers)
+
+numbers.append(7) # add 6
+print(numbers)
+
+numbers.insert(0, -1)  #insert -1, before index 0
+print(numbers)
+
+numbers.remove(3) #removes 3
+print(numbers)
+
+print(1 in numbers) #return bool of TRUE/FALSE if condition meet.
+
+numbers.clear()
+print(numbers)
