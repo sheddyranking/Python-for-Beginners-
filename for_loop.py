78729338e4958b5032f35efa65f8ba62d58de82b
sheddyranking <shedyisaac@gmail.com>
2022-01-15 09:19:56 +0100

    added for_loop

---
+++ for_loop.py
@@ -0,0 +1,18 @@
+numbers = [1, 2, 3, 4, 5]
+
+for item in numbers: #item collects each number store them in array and print them.
+    print(item)
+    item = item+1
+
+#this can also be archived with a while loop but it will take longer codes
+
+print("  ")
+
+i = 1
+while(i<= len(numbers)):
+    print(i)
+    i = i+1
+
+"""""
+the for_loop makes code much easier 
+"""""
