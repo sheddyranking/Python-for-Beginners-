270959233b117343d48892d02ed5eae49ee9a51b
sheddyranking <shedyisaac@gmail.com>
2022-01-15 09:37:09 +0100

    added the range

---
+++ range_function.py
@@ -0,0 +1,32 @@
+numbers = range(5)
+
+for number in numbers:  #the for_loop function can be use in  any numbers involve sequence. 
+    print(number)
+
+
+print(" ")
+
+#choosing a starting point.
+
+figures = range(5, 10)
+
+for each in figures:
+    print(each)
+
+print(" ")
+
+#choosing a stepping point.
+
+items = range(0, 10, 2)
+
+for item in items: #the progam returns only even number.
+    print(item)
+
+print(" ")
+
+#another simpler way of writting the range is including it in the for_loop
+
+for a in range(5):
+    print(a)
+
+    
